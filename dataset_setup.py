8f54069009cecec75c2255c9c31db53f08ce6d1d
Hyunseo Lee <iam@hsl.ee>
2023-06-06 19:32:38 +0900

    UPDATE: Add basic signature

---
+++ dataset_setup.py
@@ -0,0 +1,11 @@
+# AI프로젝트입문 (ECE30007-02)
+# Spring, 2023
+
+# Team Project: 미드바르 뿌리 이미지 인식 챌린지
+# dataset_setup.py
+# - Author: 이현서(ID# 22100600) <hslee@handong.ac.kr>
+
+# This script is for setting up the dataset.
+
+import os
+import shutil
